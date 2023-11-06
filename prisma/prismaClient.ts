5fd57ecf4081e364045003a4eb20b41d184de39e
Hemanth788 <63145553+Hemanth788@users.noreply.github.com>
2023-11-06 06:04:10 +0530

    Update prismaClient.ts

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,11 +1,21 @@
 import { Prisma, PrismaClient } from '@prisma/client'
 import { DefaultArgs } from '@prisma/client/runtime/library';
+import { tmpdir } from 'os'
+import fs from 'fs'
 
 let db: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
 
 declare global {
     var db: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
 }
+
+ fs.writeFile(
+        `${tmpdir()}/server-ca.pem`,
+        process.env.CLIENT_CERTIFICATE!,
+        err => {
+            if (err) return console.log(err)
+        }
+    )
 
 //check if we are running in production mode
 if (process.env.NODE_ENV === 'production') {
